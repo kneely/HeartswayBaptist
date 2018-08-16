e33616effd685421c4aa9dd1ae2acb30063c9f66
Kevin Neely <kevin.r.neely@outlook.com>
2018-08-15 21:20:53 -0400

    Add project files.

---
+++ HeartswayBaptist/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeartswayBaptist.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
